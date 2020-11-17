12084f5e6041493f38631cc782a46683363df95d
Myunghwa <minghoa@naver.com>
2020-11-18 11:07:31 +1300

    Add checking auth on get goal progress API

--- apis/goal/get_goal_progress.php
+++ apis/goal/get_goal_progress.php
@@ -3,31 +3,41 @@ require_once('../../common/header.php');
 require_once('../../database/goal_query.php');
 
 try {
+    $jwtCls = new Jwt();
 	
     $userSeqNo = $obj['userSeqNo'];
     $goalDate = $obj['goalDate'];
 
-	$result = getGoalProgress($userSeqNo, $goalDate);
-	$numResults = mysqli_num_rows($result);
-	
-    $counter = 0;
+    $keepLogin = $obj['keepLogin'];
+    $jwt = $obj['jwt'];
 
-	if($numResults > 0) {
+    $auch = $jwtCls->dehashing($jwt, $userSeqNo);
+    
+    if($auch) {
 
-        echo '{"result":"success", "goalProgress":[';
+        $result = getGoalProgress($userSeqNo, $goalDate);
+        $numResults = mysqli_num_rows($result);
+        
+        $counter = 0;
 
-        $goalSeqNo = "";
-        $readingBible = "";
-        while($row = mysqli_fetch_assoc($result)){
-            echo json_encode($row);
-			if (++$counter != $numResults) {
-				echo",";
-			}
+        if($numResults > 0) {
+
+            echo '{"result":"success", "goalProgress":[';
+
+            $goalSeqNo = "";
+            $readingBible = "";
+            while($row = mysqli_fetch_assoc($result)){
+                echo json_encode($row);
+                if (++$counter != $numResults) {
+                    echo",";
+                }
+            }
+            echo ']}';
         }
-		echo ']}';
-    }
-    else {
-        echo '{"result":"fail", "errorCode": "01", "errorMessage": "There are no goals in progress."}';
+        else {
+            echo '{"result":"fail", "errorCode": "01", "errorMessage": "There are no goals in progress."}';
+        }
+
     }
 
 }
