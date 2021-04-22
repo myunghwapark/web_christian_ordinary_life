468b0dfbac2592d92fe35d6ec70a80ea9823c6bf
Myunghwa Park <mocaming@gmail.com>
2021-04-22 15:55:45 +1200

    inthefridge privacy policy

---
+++ web/inthefridge_privacy_policy.php
@@ -0,0 +1,26 @@
+<?php
+$language = $_GET["language"];
+
+?>
+
+<!DOCTYPE html>
+    <html>
+    <head>
+      <meta charset='utf-8'>
+      <meta name='viewport' content='width=device-width'>
+      <title>Privacy Policy</title>
+      <style> body { font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; padding:1em; } </style>
+    </head>
+    <body>
+      <?php
+
+      if($language == 'ko') {
+        include_once 'inthefridge_privacy_policy_ko.php';
+      }
+      else {
+        include_once 'inthefridge_privacy_policy_en.php';
+      }
+      ?>
+    
+    </body>
+    </html>
---
+++ web/inthefridge_privacy_policy_en.php
@@ -0,0 +1,64 @@
+<strong>Privacy Policy</strong> <p>
+    <p>
+        InTheFridge is provided by Myunghwa Park.<br/>
+        InTheFridge places great importance on the protection of users&apos;  personal information, and I am doing my best to ensure that the personal information provided to me online is protected while users use my services.<br/><br/>
+
+        Through the privacy policy, I inform you of the purposes and methods of using the personal information provided by users, and what measures are being taken to protect personal information.<br/><br/>
+
+        
+        InTheFridge&apos; s privacy policy may be changed from time to time due to changes in government laws and guidelines or changes in our internal policy, and can be viewed at any time in the settings menu on the app.<br/>
+  </p> 
+  <p><strong>Purpose of collection and use of personal information</strong></p> 
+  <p>
+  InTheFridge collect and use the personal information of users for the following purposes in order to provide better quality services.</p>
+
+    <ul>
+        <li>Require camera usage and album permission: Using cameras and albums, users can replace icons with photos of food they want.</li>
+        <li>Push notification permission request: Helps the user to manage food in the refrigerator by using the notification function.</li>
+    </ul>
+   
+  <div><p>
+  The app does use third party services that may collect information used to identify you.
+    </p> <p>
+    Link to privacy policy of third party service providers used by the app
+    </p> 
+    <ul>
+        <li><a href="https://www.google.com/policies/privacy/" target="_blank" rel="noopener noreferrer">Google Play Services</a></li>
+        <li><a href="https://support.google.com/admob/answer/6128543?hl=en" target="_blank" rel="noopener noreferrer">AdMob</a></li>
+    </ul>
+</div> 
+
+  </p> 
+  <p><strong>Cookies</strong></p> <p>
+  Cookies are files with a small amount of data that are
+    commonly used as anonymous unique identifiers. These are sent
+    to your browser from the websites that you visit and are
+    stored on your device's internal memory.
+  </p> 
+  <p>
+    This app provides a more convenient service by storing alarm time and app setting information using cookies.
+  </p> 
+  
+  <p><strong>Service Providers</strong></p> <p>
+  I may employ third-party companies and
+    individuals due to the following reasons:
+  </p> 
+  
+  <ul>
+    <li>To facilitate our Service;</li>
+    <li>To provide the Service on our behalf;</li> 
+    <li>To perform Service-related services; or</li> 
+    <li>To assist us in analyzing how our Service is used.</li>
+</ul> 
+   
+     
+<p><strong>Children’s Privacy</strong></p> <p>
+        Users must be 14 or older to be eligible to use InTheFridge application.
+        I do not knowingly collect personally identifiable information from children under 13. 
+        If a parent or guardian becomes aware that his or her child has provided us with information without their consent, he or she should contact us at mocaming@gmail.com.  
+      </p> 
+  <p>This policy is effective as of 2021-04-22</p> 
+  <p><strong>Contact Us</strong></p> <p>
+    If you have any questions or suggestions about the
+    Privacy Policy, do not hesitate to contact us at mocaming@gmail.com.
+  </p> 
